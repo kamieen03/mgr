4b323790a30aa3fb2306b86fe01719ae9d06c156
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2021-04-29 14:08:31 +0200

    Simple brigtness invariant resnet18

--- architectures/__init__.py
+++ architectures/__init__.py
@@ -1,2 +1,3 @@
 from .plain_resnet18 import PlainResNet18
 from .gresnet18 import GResNet18
+from .brightness_resnet18 import BResNet18
---
+++ run_bresnet.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import torchvision
+import torch
+from architectures import BResNet18
+import sys
+
+def make_data(data_path):
+    train_transforms = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Pad(2),
+        torchvision.transforms.RandomCrop(88),
+        torchvision.transforms.RandomHorizontalFlip(p=0.5)
+    ])
+    test_transforms = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+    ])
+    _train_data = torchvision.datasets.STL10(data_path, split='train', download=False,
+                transform = train_transforms)
+    _test_data = torchvision.datasets.STL10(data_path, split='test', download=False,
+                transform = test_transforms)
+    train_data_loader = torch.utils.data.DataLoader(_train_data,
+                                              batch_size=128,
+                                              shuffle=True,
+                                              num_workers=2)
+    test_data_loader = torch.utils.data.DataLoader(_test_data,
+                                              batch_size=128,
+                                              shuffle=True,
+                                              num_workers=2)
+    return train_data_loader, test_data_loader
+
+def train(net, opt, lossf, data):
+    losses = []
+    good, total = 0, 0
+    net.train()
+    for X, y in data:
+        #torchvision.transforms.functional.to_pil_image(X[0]).show()
+        #input()
+        X, y = X.cuda(), y.cuda()
+        opt.zero_grad()
+        out = net(X)
+        loss = lossf(out, y)
+        losses.append(loss.item())
+        loss.backward()
+        opt.step()
+        good += sum(out.argmax(1) == y)
+        total += len(y)
+    print('Train Loss:', torch.tensor(losses).float().mean())
+    print('Train Accuracy:', good/total*100)
+
+def test(net, lossf, data):
+    losses = []
+    good, total = 0, 0
+    net.eval()
+    with torch.no_grad():
+        for X, y in data:
+            X, y = X.cuda(), y.cuda()
+            out = net(X)
+            loss = lossf(out, y)
+            losses.append(loss.item())
+            good += sum(out.argmax(1) == y)
+            total += len(y)
+    print('Test Loss:', torch.tensor(losses).float().mean())
+    print('Test Accuracy:', good/total*100)
+
+def main(save_path, data_path):
+    EPOCHS = 200
+    net = BResNet18().cuda()
+    opt = torch.optim.AdamW(net.parameters(), weight_decay=0.02)
+    lossf = torch.nn.CrossEntropyLoss()
+    train_data, test_data = make_data(data_path)
+    for epoch in range(EPOCHS):
+        print('Epoch:', epoch)
+        train(net, opt, lossf, train_data)
+        test(net, lossf, test_data)
+        torch.save(net.state_dict(), save_path)
+
+if __name__ == '__main__':
+    save_path = 'models/plain_bresnet18.pth'
+    data_path = 'data/stl10'
+    if len(sys.argv) > 1 and sys.argv[1] == 'g':
+        save_path = '/content/drive/MyDrive/mgr/models/plain_bresnet18.pth'
+        data_path = '/content/drive/MyDrive/mgr/stl10'
+    main(save_path, data_path)
+
