259cd9c6266166155acc4d35c9a260b7cc3ec92d
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2021-06-18 13:51:17 +0200

    Add training continuation

--- run_all_models.py
+++ run_all_models.py
@@ -92,7 +92,31 @@ def test(net, lossf, data, net_name):
             total += len(y)
     return torch.tensor(losses).float().mean().item(), (good/total*100).item()
 
-def main():
+def load_run_results(fname):
+    with open(fname, 'r') as f:
+        lines = f.readlines()
+    tr_L, tr_Acc = lines[0], lines[1]
+    te_L, te_Acc = lines[2], lines[3]
+    tr_L = [float(c) for c in tr_L[1:-2].split(',')]
+    tr_Acc = [float(c) for c in tr_Acc[1:-2].split(',')]
+    te_L = [float(c) for c in te_L[1:-2].split(',')]
+    te_Acc = [float(c) for c in te_Acc[1:-2].split(',')]
+    return tr_L, te_L, tr_Acc, te_Acc
+
+def main(argv):
+    if argv[1] == 'True':
+        JITTER = True
+    elif argv[1] == 'False':
+        JITTER = False
+    else:
+        raise Exception('Wrong JITTER argument')
+    START_MODEL_NAME = None
+    start = True
+    if len(argv) > 2:
+        START_MODEL_NAME = argv[2]
+        START_EPOCH = int(argv[3])
+        start = False
+
     EPOCHS = 150
     base_model_path = '/content/drive/MyDrive/mgr/models'
     base_runs_path = '/content/drive/MyDrive/mgr/runs'
@@ -100,19 +124,26 @@ def main():
     #base_model_path = 'models'
     #base_runs_path = 'runs'
     #data_path = 'data/stl10'
-    for JITTER in [False, True]:
-        train_data, test_data = make_data(data_path, JITTER)
-        for net, net_name in model_list():
-            net = net.cuda()
+    train_data, test_data = make_data(data_path, JITTER)
+    lossf = torch.nn.CrossEntropyLoss()
+    for net, net_name in model_list():
+        model_path = f'{base_model_path}/{net_name}_{JITTER}.pth'
+        runs_path = f'{base_runs_path}/{net_name}_{JITTER}.txt'
+        tr_L, te_L, tr_Acc, te_Acc = [], [], [], []
+        net = net.cuda()
+        if START_MODEL_NAME == net_name:
+            net.load_state_dict(torch.load(model_path))
+            tr_L, te_L, tr_Acc, te_Acc = load_run_results(runs_path)
+            start = True
+
+        if start:
             opt = torch.optim.AdamW(net.parameters(), weight_decay=0.02)
-            lossf = torch.nn.CrossEntropyLoss()
-            tr_L, te_L, tr_Acc, te_Acc = [], [], [], []
-            for epoch in range(EPOCHS):
+            for epoch in range(START_EPOCH, EPOCHS):
                 print(net_name, JITTER, epoch)
                 train_loss, train_acc = train(net, opt, lossf, train_data,
                         net_name)
                 test_loss, test_acc = test(net, lossf, test_data, net_name)
-                torch.save(net.state_dict(), f'{base_model_path}/{net_name}_{JITTER}.pth')
+                torch.save(net.state_dict(), model_path)
 
                 tr_L.append(train_loss); tr_Acc.append(train_acc)
                 te_L.append(test_loss); te_Acc.append(test_acc)
@@ -121,7 +152,8 @@ def main():
                     f.write(str(tr_Acc)+'\n')
                     f.write(str(te_L)+'\n')
                     f.write(str(te_Acc)+'\n')
+            START_EPOCH = 0
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
 
