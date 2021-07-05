e10b0375ae08f8adbbf42652ea0af7f80aee0d50
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2021-07-05 18:27:56 +0200

    Remove exploding activation function from GammaEq

--- architectures/groups.py
+++ architectures/groups.py
@@ -253,7 +253,6 @@ class RplusGamma(Group):
         return RplusGamma.transform_tensor(kernel, factor)
 
     def activation():
-        return lambda x: torch.sign(x) * torch.abs(x)**0.3
         return nn.Softsign()
 
 
